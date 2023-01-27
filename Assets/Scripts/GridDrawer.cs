611ecfe27493f105b1768fa8d8e2e6e021047445
Imilkowski <58135969+Imilkowski@users.noreply.github.com>
2023-01-27 15:51:27 +0100

    v0.02
    Initializing main array

--- Assets/Scripts/GridDrawer.cs
+++ Assets/Scripts/GridDrawer.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class GridDrawer : MonoBehaviour
 {
+    [Header("Assignables")]
     [SerializeField] private RectTransform _boardBackground;
+    [SerializeField] private RectTransform _wallsParent;
 
     [Header("Prefabs")]
     [SerializeField] private GameObject _wallPrefab;
@@ -14,5 +16,12 @@ public class GridDrawer : MonoBehaviour
     public void SetBackgroundSize(Vector2Int newSize)
     {
         _boardBackground.sizeDelta = newSize;
+    }
+
+    public void DrawWalls()
+    {
+        //spawn walls
+        Instantiate(_wallPrefab, GridManager.Instance.startPos, Quaternion.identity, _wallsParent);
+        //TODO: instead of spawning in world pos, set the local pos from parent
     }
 }
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    public const int ELEMENT_SIZE = 32;
+
+    public Vector2Int gridSize { get; private set; }
+    public Vector3 startPos { get; private set; }
+
+    public enum ElementType
+    {
+        Empty,
+        Wall,
+        Item
+    }
+
+    public class GridElement
+    {
+        public ElementType type;
+        public GameObject heldElement;
+
+        public GridElement(ElementType type, GameObject heldElement)
+        {
+            this.type = type;
+            this.heldElement = heldElement;
+        }
+    }
+
     public static GridManager Instance;
 
     [Header("Assignables")]
     [SerializeField] private GridDrawer gridDrawerComponent;
+
+    private GridElement[,] gridElementsArray;
 
     void Awake()
     {
@@ -24,6 +50,62 @@ public class GridManager : MonoBehaviour
     //initializes the array containg all the needed data and a visual grid
     public void InitializeGrid(Vector2Int gridSize)
     {
+        this.gridSize = gridSize;
         gridDrawerComponent.SetBackgroundSize(gridSize * 32);
+
+        startPos = CalculateStartPos();
+
+        gridElementsArray = new GridElement[gridSize.y, gridSize.x];
+        for (int i = 0; i < gridSize.y; i++)
+        {
+            for (int j = 0; j < gridSize.x; j++)
+            {
+                gridElementsArray[i, j] = new GridElement(ElementType.Empty, null);
+            }
+        }
+
+        SetUpWalls();
+    }
+
+    private Vector3 CalculateStartPos()
+    {
+        Vector3 pos = new Vector3();
+
+        if(gridSize.x % 2 == 0)
+        {
+            pos.x = (gridSize.x / 2) * ELEMENT_SIZE - (int)(ELEMENT_SIZE * 0.5f);
+        }
+        else
+        {
+            pos.x = -(int)Mathf.Floor(gridSize.x / 2) * ELEMENT_SIZE;
+        }
+
+        if (gridSize.y % 2 == 0)
+        {
+            pos.y = (gridSize.y / 2) * ELEMENT_SIZE - (int)(ELEMENT_SIZE * 0.5f);
+        }
+        else
+        {
+            pos.y = -(int)Mathf.Floor(gridSize.y / 2) * ELEMENT_SIZE;
+        }
+
+        return pos;
+    }
+
+    //puts walls to the gridElementsArray
+    private void SetUpWalls()
+    {
+        for (int i = 0; i < gridSize.y; i++)
+        {
+            for (int j = 0; j < gridSize.x; j++)
+            {
+                if (Random.Range(0f, 1f) <= 0.25f)
+                {
+                    gridElementsArray[i, j].type = ElementType.Wall;
+                }
+            }
+        }
+
+        gridDrawerComponent.DrawWalls();
     }
 }
